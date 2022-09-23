058f4f53aba79df4a000c3350329e60167ae55f1
Ensar CAN <ensarcan57@gmail.com>
2022-09-23 22:06:01 +0300

    Java Ders1

---
+++ src/oop1/Product.java
@@ -0,0 +1,52 @@
+package oop1;
+
+// class'lar PascalCase sitiliyle isimlendirilir.
+public class Product {
+	private String name;
+	// field'lar camelCase yazılır.
+	private double unitPrice;
+	private double discount;
+	private String imageUrl;
+	private int unitInStock;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getUnitPrice() {
+		return unitPrice;
+	}
+
+	public void setUnitPrice(double unitPrice) {
+		this.unitPrice = unitPrice;
+	}
+
+	public double getDiscount() {
+		return discount;
+	}
+// set okuma get yazma
+	public void setDiscount(double discount) {
+		this.discount = discount;
+	}
+
+	public String getImageUrl() {
+		return imageUrl;
+	}
+
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+
+	public int getUnitInStock() {
+		return unitInStock;
+	}
+
+	public void setUnitInStock(int unitInStock) {
+		this.unitInStock = unitInStock;
+	}
+
+}
